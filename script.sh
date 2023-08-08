01203831e299cf8a8f9cd8c0ea9cdc53c4845c61
Mateusz Kubiak <matkubiak888@gmail.com>
2023-08-08 17:27:28 +0200

    feat: config script for additional software

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ read -p "Now, are you sure you want to continue? [y/n] " -n 1 -r
 echo " "
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo "Script canceled."
-  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
 
@@ -68,7 +68,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 fi
 
 instl_yay $hypr_pack waybar-hyprland-git swww-git
-instl_pacman alacritty wofi dunst polkit-kde-agent xdg-desktop-portal-hyprland cliphist
+instl_pacman alacritty wofi dunst polkit-kde-agent xdg-desktop-portal-hyprland cliphist hyprpicker
 
 
 # AUDIO
@@ -83,44 +83,13 @@ echo "Installing fonts and emoji..."
 instl_yay ttf-twemoji ttf-jetbrains-mono-nerd
 
 
-# COLOR PICKER
-echo "Installing color picker..."
-instl_pacman hyprpicker
-
-
 # ADDITIONALS
 echo "Installing additional software..."
 thunar_pack="thunar gvfs thunar-volman gvfs-mtp tumbler ffmpegthumbnailer"
 media_pack="viewnior vlc"
 cli_pack="ranger htop alsa-utils vim neovim"
-gui_pack="firefox ark gparted keepassxc qbittorrent"
+gui_pack="firefox ark gparted"
 instl_pacman $thunar_pack $media_pack $cli_pack $gui_pack
-
-read -p "Do you want to install Joplin, Anki and Webcord? [y/n] " -n 1 -r
-echo " "
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Installing Joplin, Anki and Webcord..."
-  instl_yay joplin-desktop anki webcord
-fi
-
-
-# DEVELOPMENT
-read -p "Do you want to install development tools? [y/n] " -n 1 -r
-echo " "
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  echo "Installing development tools..."
-  instl_yay vscodium-bin
-  instl_pacman virtualbox
-fi
-
-
-# STEAM
-# echo "Installing steam ..."
-
-
-# LIBRE OFFICE
-# echo "Installing libre office"
-# instl_pacman libreoffice
 
 
 # DOT FILES
@@ -130,3 +99,12 @@ cp -r ./config/eww ~/.config/eww
 cp -r ./config/hypr ~/.config/hypr
 cp -r ./config/waybar ~/.config/waybar
 sudo cp -r ./wallpapers /usr/share/wallpapers
+
+
+# CONFIG
+read -p "Do you want to install additional software from config.sh? [y/n] " -n 1 -r
+echo " "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  nano ./config.sh
+  bash ./config.sh
+fi
