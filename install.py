4a9116a15b22c49b5545e2b34efd6a382475b247
Mateusz Kubiak <matkubiak888@gmail.com>
2023-12-10 23:09:49 +0100

    refactor: better messaging naming

--- install.py
+++ install.py
@@ -12,15 +12,15 @@ class paths:
     config_file = os.path.join(config_dir, 'config.ini')
     log_file = os.path.join(config_dir, 'log')
 
-def message(mode: str, message: str):
+def message(mode: str, mes: str):
     headers = {
         'info' : '\033[1;36m[INFO]\033[0m',
-        'good' : '\033[1;32m[GOOD]\033[0m',
+        'success' : '\033[1;32m[SUCCESS]\033[0m',
         'debug' : '\033[1;35m[DEBUG]\033[0m',
-        'warn' : '\033[1;33m[WARNING]\033[0m',
+        'warning' : '\033[1;33m[WARNING]\033[0m',
         'error': '\033[1;31m[ERROR]\033[0m'
     }
-    print(headers[mode] + ' ' + message)
+    print(headers[mode] + ' ' + mes)
 
 def has_internet_access():
     try:
@@ -61,13 +61,13 @@ def main():
     if not has_internet_access():
         message('error', 'Internet not connected!')
         exit(1)
-    message('good', 'Internet connected!')
+    message('success', 'Internet connected!')
 
     # WELCOME
     print('')
     message('info', 'Hello! This script will install the whole Hyprland ecosystem along with software specified in config.ini.')
     message('info', 'If something goes wrong, look for the log file in the script\'s directory.')
-    message('warn', 'If you aren\'t sure what software the script will install and whether you want it, please consult with the README')
+    message('warning', 'If you aren\'t sure what software the script will install and whether you want it, please consult with the README')
     if input("Do you want to continue? (y/n) ") != 'y':
         exit()
 
